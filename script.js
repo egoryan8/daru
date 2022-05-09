bfcdd31a40076d77083442802c28f5abbff2cc7f
egoryan8 <fogel_92@bk.ru>
2022-05-09 16:53:40 +0700

    fix burger menu

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const menuList = document.querySelector(".menu-list");
+const headerWrapper = document.querySelector(".header-wrapper");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active-h");
   menuList.classList.toggle("active-m");
+  headerWrapper.classList.toggle("active");
 })
 
 document.querySelectorAll(".menu-list__link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active-h");
   menuList.classList.remove("active-m");
+  headerWrapper.classList.remove("active");
 }))
