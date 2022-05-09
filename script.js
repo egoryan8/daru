dd4d423d445e5e06981095a536eced145efdaf47
egoryan8 <fogel_92@bk.ru>
2022-05-09 17:08:44 +0700

    fix classes

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const menuList = document.querySelector(".menu-list");
 const headerWrapper = document.querySelector(".header-wrapper");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active-h");
-  menuList.classList.toggle("active-m");
+  hamburger.classList.toggle("active");
+  menuList.classList.toggle("active");
   headerWrapper.classList.toggle("active");
 })
 
 document.querySelectorAll(".menu-list__link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active-h");
-  menuList.classList.remove("active-m");
+  hamburger.classList.remove("active");
+  menuList.classList.remove("active");
   headerWrapper.classList.remove("active");
 }))
